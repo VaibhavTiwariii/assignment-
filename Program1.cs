4b4a5c6b6d67b6fbd753f1e35b82e30165b7b13c
VaibhavTiwariii <138752219+VaibhavTiwariii@users.noreply.github.com>
2024-05-20 15:08:22 +0530

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace pritingtriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the desired height of the triangle:-");
+            if (int.TryParse(Console.ReadLine(), out int height_of_triangle) && height_of_triangle > 0)
+            {
+                PrintingTriangle(height_of_triangle);
+            }
+            else
+            {
+                Console.WriteLine("enter the positive input not negative");
+            }
+        }
+
+        static void PrintingTriangle(int height_of_triangle)
+        {
+            for (int i = 1; i <= height_of_triangle; i++)
+            {
+                
+                for (int j = 0; j < height_of_triangle - i; j++)
+                {
+                    Console.Write(" ");
+                }
+              
+                for (int k = 0; k < (2 * i - 1); k++)
+                {
+                    Console.Write("*");
+                }
+               
+                Console.WriteLine();
+            }
+        }
+    }
+}
